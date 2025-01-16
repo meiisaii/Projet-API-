4d745d06c0181b934b6be6aa7faedae96d1a1982
elsarrr77 <elsa.rjd974@gmail.com>
2025-01-16 14:07:54 +0100

    ajout users

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ const port = 3000;
 
 app.use(express.json());
 
+// DATA USERS
+
+const users = [
+    {
+        id: 1,
+        name: "Valentine",
+        email: "valentine@gmail.com",
+        age: 20,
+    },
+
+    {
+        id: 2,
+        name: "Jason",
+        email: "jason@gmail.com",
+        age: 26,
+    },
+
+    {
+        id: 3,
+        name: "Sacha",
+        email: "sacha@gmail.com",
+        age: 22,
+    }
+]
+
