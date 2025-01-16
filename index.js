8bf6b5b8d17423bc57e5a6d5ad38c6473a8f005b
elsarrr77 <elsa.rjd974@gmail.com>
2025-01-16 12:54:27 +0100

    Premier commit test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-salut c'est un test pour github
+const express = require ("express");
+const mysql = require ("mysql2");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
