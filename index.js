3a59e6c8bc5121cdffe5996d070bdb07de0c2858
Hugo DURAND <hugo@MacBook-Air-de-Hugo.local>
2025-01-16 16:27:54 +0100

    Commit modules

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const mysql = require ("mysql2");
 //Créer une app express
 const app = express();
 
-//Définition du port
-const port = 3000;
+//connexion mysql
+const connexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', 
+    database: 'user_db',
+});
 
 app.use(express.json());
 
@@ -36,18 +41,15 @@ const users = [
 ]
 
 //routes
-//
+//route d'origine
+app.get("/", (req, res) => {
+    res.send("Bienvenue sur notre API :)");
+});
 
-
-
-
-
-
-
-
-
-
-
+//route pour récupérer la liste des utilisateurs
+app.get("/api/users", (req, res) => {
+    res.json(users);
+});
 
 
 // Ajouter un utilisateur 
@@ -62,4 +64,53 @@ app.post ("/api/users", (req, res) => {
 // Ajouter l'utilisateur à la liste
     users.push(newUser);
 
+});
+
+// Mettre à jour un utilisateur 
+
+app.put("/api/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id); //Extraire l'id de l'utilisateur depuis les param de la req
+    const existingUser = users.find((user) => user.id === userId); //Trouver l'utilisateur correspondant à l'ID de la req
+ 
+    if (existingUser) {
+        existingUser.name = req.body.name || existingUser.name;
+        existingUser.email = req.body.email || existingUser.email;
+        existingUser.age = req.body.age || existingUser.age;
+        res.json(existingUser);
+    } else {
+        res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+});
+
+//route pour supprimer un utilisateur
+app.delete("/api/users/:id", (req, res) => {
+    const userId = req.params.id;
+
+    if (!userId) {
+        return res.status(400).json({ message: "un ID d'utilisateur requis" });
+    }
+
+    const query = 'DELETE FROM users WHERE id = ?';
+
+    connexion.query(query, [userId], (error, result) => {
+        if (error) {
+            return res.status(500).json({ message: "Erreur de suppression", error });
+        }
+        
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Utilisateur non trouvé !" });
+        }
+
+        res.status(200).json({ message: "Utilisateur supprimé :/"});
+    });
+});
+
+
+
+
+
+//définition du port et démarrage du serveur
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Serveur en écoute sur http://localhost:${port}`);
 });
